10364834bf7dbd6199ae79bbb4aed942ddd6357a
Stack Soumik <110479389+Soum-ik@users.noreply.github.com>
2024-01-21 23:16:24 +0600

    fix: navbar socail media

--- src/components/Navbar/NavbarContact.jsx
+++ src/components/Navbar/NavbarContact.jsx
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
-import {
-  IoLogoFacebook,
-  IoLogoGithub,
-  IoLogoLinkedin,
-  IoLogoTwitter,
-  IoLogoYoutube,
-} from "react-icons/io5";
-import Navlink from "./NavbarLinks";
+import FollowUs from "../Footer/FollowUs";
+
+import { IoLogoFacebook, IoLogoLinkedin, IoLogoYoutube } from "react-icons/io5";
+import { BsInstagram, BsTelegram, BsTwitterX } from "react-icons/bs";
+import { useFetch } from "../../libs/fetchData";
+
 const NavContact = () => {
+  const source = useFetch("http://localhost:5000/social/links");
+
   return (
     <div>
       <nav className=" mt-3 sm:flex items-center justify-between hidden font-Headingfont">
@@ -30,40 +30,13 @@ const NavContact = () => {
             <span>|</span>
             <Link>Singup Now</Link>
           </div>
-          <div className=" flex space-x-2 text-[25px] ">
-            <Navlink
-              path={`https://www.facebook.com/farjaxtci`}
-              icon={
-                <IoLogoFacebook className="border-gray-700  border rounded-full  border-dotted p-[3px]" />
-              }
-            />
-
-            <Navlink
-              path={`/`}
-              icon={
-                <IoLogoTwitter className="border-gray-700  border rounded-full  border-dotted p-[3px]" />
-              }
-            />
-
-            <Navlink
-              path={`/`}
-              icon={
-                <IoLogoYoutube className="border-gray-700  border rounded-full  border-dotted p-[3px]" />
-              }
-            />
-            <Navlink
-              path={`https://www.linkedin.com/in/saidur-md-rahman-40a7b4211/`}
-              icon={
-                <IoLogoLinkedin className="border-gray-700  border rounded-full  border-dotted p-[3px]" />
-              }
-            />
-
-            <Navlink
-              path={`https://github.com/farjaxiot`}
-              icon={
-                <IoLogoGithub className="border-gray-700  border rounded-full  border-dotted p-[3px]" />
-              }
-            />
+          <div className=" flex items-center justify-center gap-3">
+            <FollowUs icon={<IoLogoFacebook />} link={source.facebook} />
+            {/* <FollowUs icon={<BsTwitterX />} link={source.twitter} /> */}
+            <FollowUs icon={<IoLogoLinkedin />} link={source.linkedin} />
+            <FollowUs icon={<IoLogoYoutube />} link={source.youtube} />
+            {/* <FollowUs icon={<BsInstagram />} link={source.instagram} /> */}
+            {/* <FollowUs icon={<BsTelegram />} link={source.telegram} /> */}
           </div>
         </div>
       </nav>
