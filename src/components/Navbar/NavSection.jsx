7252abb9f1f26101ff8f4498079f482e0bfc5ebe
Stack Soumik <110479389+Soum-ik@users.noreply.github.com>
2024-01-25 02:25:48 +0600

    feat: add dropdown course

--- src/components/Navbar/NavSection.jsx
+++ src/components/Navbar/NavSection.jsx
@@ -1,17 +1,45 @@
 import { Link } from "react-router-dom";
 import Layout from "../Layout/layout";
 import WhatsAppLink from "../WhatsApp";
-import { Button, Dropdown } from "antd";
+
+import { useEffect, useState } from "react";
 const Section = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/choosepath");
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array means this effect runs once when the component mounts
+
+  const names = data.map((item) => item.name);
+  console.log(names);
   return (
     <Layout>
       <nav className=" font-Headingfont flex justify-between items-center text-white">
         <ul className=" flex justify-center items-center space-x-7">
-          <Dropdown menu={{}} placement="bottomLeft">
-            <Button className="text-white hover:text-black outline-none border-none focus:outline-none active:outline-none hover:outline-none text-[16px] focus:border-none">
+          <div className="relative inline-block group">
+            <button className="text-white   px-4 py-2 rounded-md focus:outline-none   transition duration-300">
               COURSE
-            </Button>
-          </Dropdown>
+            </button>
+            <div className="absolute rounded-md p-2 hidden bg-color shadow-md mt-2 group-hover:block">
+              {/* Your dropdown content goes here */}
+              {names.map((name, index) => (
+                <li className=" text-white" key={index}>
+                  {index}:{name}
+                  <br />
+                </li>
+              ))}
+            </div>
+          </div>
 
           <Link to={"/aboutus"} className="cursor-pointer">
             ABOUT US
