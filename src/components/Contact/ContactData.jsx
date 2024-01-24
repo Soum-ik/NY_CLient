2cb1046e023fe0169f36a35148dfa15626512cab
Stack Soumik <110479389+Soum-ik@users.noreply.github.com>
2024-01-25 03:03:30 +0600

    feat: contact form

--- src/components/Contact/ContactData.jsx
+++ src/components/Contact/ContactData.jsx
@@ -1,65 +1,100 @@
- 
-import ContactInp from "./ContactForm";
+import { useState } from "react";
 
-const ContactData = () => { 
+const ContactData = () => {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+    number: "",
+  });
 
-  function sendEmail(e) {
+  const [submitted, setSubmitted] = useState(false);
+
+  const sendEmail = (e) => {
     e.preventDefault();
+    // Perform your email sending logic here
+    // For now, let's just show a "Thank you" message
+    alert("Thank You, Your Data Submit SuccessFully")
+    setSubmitted(true);
+  };
 
-    alert("Thank you!");
-  }
+  const handleState = (e, field) => {
+    setForm({
+      ...form,
+      [field]: e.target.value,
+    });
+  };
+
+  const isFormValid = () => {
+    return (
+      form.name.trim() !== "" &&
+      form.email.trim() !== "" &&
+      form.message.trim() !== ""
+    );
+  };
 
   return (
     <form
-      //   ref={form}
       onSubmit={sendEmail}
-      className="sm:col-span-7 grid-cols-1  lg:col-span-8 sm:min-w-[400px] md:min-w-[600px] h-full flex flex-col justify-between "
+      className="sm:col-span-7 grid-cols-1  lg:col-span-8 sm:min-w-[400px] md:min-w-[600px] h-full flex flex-col justify-between shadow-2xl p-2"
     >
-      <div className="grid gap-4 grid-cols-1 lg:grid-cols-2 w-full mt-4 md:mt-0">
-        <ContactInp
-          placeholder="Your Name"
-          type="text"
-          name="from_name"
-          required
-          // value={fromData?.name || ""}
-        />
-        <ContactInp
-          placeholder="Your Email"
-          type="email"
-          name="email"
-          required
-        />
-        <ContactInp
-          placeholder="Your phone number (optional)"
-          type="text"
-          name="phone"
-        />
-        <ContactInp placeholder="text" type="text" name="subject" />
-      </div>
-      <div className="w-full  my-4 flex-1">
-        <div>
-          <textarea
-            className="w-full  rounded-md shadow-md p-3 border-1 border-gray-200 text-black focus:border-transparent focus:outline-transparent focus:ring-0 min-h-[200px] md:min-h-full lg:min-h-full"
-            placeholder="Your Message"
-            name="message"
-            required
-          />
+      {submitted ? (
+        <div className="text-center">
+          <p className="text-xl font-bold text-green-600">
+            Thank you for submitting!
+          </p>
         </div>
-      </div>
+      ) : (
+        <>
+          <div className="grid gap-4 grid-cols-1 lg:grid-cols-2 w-full mt-4 md:mt-0">
+            <input
+              value={form.name}
+              onChange={(e) => handleState(e, "name")}
+              placeholder={"Enter your name"}
+              className={`w-full rounded-md shadow-md border-1 border-gray-200 focus:border-transparent focus:outline-transparent focus:ring-0 p-4 text-black`}
+            />
+            <input
+              value={form.email}
+              onChange={(e) => handleState(e, "email")}
+              placeholder={"Enter your email"}
+              className={`w-full rounded-md shadow-md border-1 border-gray-200 focus:border-transparent focus:outline-transparent focus:ring-0 p-4 text-black`}
+            />
+          </div>
 
-      <div>
-        <div className="text-center mt-1">
-          <button
-            type="submit"
-            className=" bg-gradient-to-r from-red-800 to-red-700 outline-none   hover:bg-gradient-to-bl font-medium rounded-3xl text-sm px-7 md:hover:px-12 py-3.5 text-center text-white transition-all disabled:opacity-75 disabled:cursor-progress"
-            name="message"
-            // disabled={loading}
-          >
-            Send Message
-          </button>
-        </div>
-      </div>
+          <div className="w-full my-4 flex-1">
+            <textarea
+              value={form.message}
+              onChange={(e) => handleState(e, "message")}
+              className="w-full rounded-md shadow-md p-3 border-1 border-gray-200 text-black focus:border-transparent focus:outline-transparent focus:ring-0 min-h-[200px] md:min-h-full lg:min-h-full"
+              placeholder="Your Message"
+              name="message"
+              required
+            />
+          </div>
+
+          <div className="w-full my-4 flex-1">
+            <input
+              value={form.number}
+              onChange={(e) => handleState(e, "number")}
+              placeholder={"Enter your number (optional)"}
+              className={`w-full rounded-md shadow-md border-1 border-gray-200 focus:border-transparent focus:outline-transparent focus:ring-0 p-4 text-black`}
+            />
+          </div>
+
+          <div className="text-center mt-1">
+            <button
+              type="submit"
+              className="bg-gradient-to-r from-red-800 to-red-700 outline-none hover:bg-gradient-to-bl font-medium rounded-3xl text-sm px-7 md:hover:px-12 py-3.5 text-center text-white transition-all disabled:opacity-75 disabled:cursor-progress"
+              name="message"
+              disabled={!isFormValid()}
+            >
+              Send Message
+            </button>
+          </div>
+        </>
+      )}
     </form>
   );
 };
+
 export default ContactData;
--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -1,22 +1,15 @@
-import styles, { layout } from "../../style/style";
-import CustomData from "../CustomData";
-import ContactData from "./ContactData";
+import Layout from "../Layout/layout";
 
+import ContactData from "./ContactData";
 
 const Contact = () => {
   return (
-    <div className={`${styles.flexCenter} ${styles.marginX} ${styles.marginY}`}>
-      <div className={`${styles.boxWidth}  `}>
-        <section className={`${layout.section} ${styles.paddingX}`}>
-            {/* left side text option*/}
-            <CustomData heading={"Request for service"} tittle={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus culpa veniam vero dignissimos perferendis illo dolor? Atque aliquid corporis ex autem sapiente quibusdam rerum maiores. Laboriosam perferendis id molestiae ex."}/>
+    <Layout>
+      <div className=" max-w-sm">
 
-            {/* right side */}
-            <ContactData/>
-        </section>
+      <ContactData />
       </div>
-    </div>
+    </Layout>
   );
 };
 export default Contact;
- 
