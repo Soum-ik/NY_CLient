6f319cff7b70136cf585ab87cbce5673b43beec3
Stack Soumik <110479389+Soum-ik@users.noreply.github.com>
2024-02-06 00:45:35 +0600

    navigation

--- src/components/Navbar/NavSection.jsx
+++ src/components/Navbar/NavSection.jsx
@@ -1,28 +1,27 @@
 import { Link } from "react-router-dom";
 import Layout from "../Layout/layout";
 import WhatsAppLink from "../WhatsApp";
-
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import config from "../../../config";
 // import config from "../../../config";
 
 const Section = () => {
-  // const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   // console.log(data[0]._id);
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await fetch(`${config.apiUrl}choosepath`);
-  //       const jsonData = await response.json();
-  //       setData(jsonData);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${config.apiUrl}choosepath`);
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
-  //     } catch (error) {
-  //       console.error("Error fetching data:", error);
-  //     }
-  //   };
-
-  //   fetchData();
-  // }, []); // Empty dependency array means this effect runs once when the component mounts
+    fetchData();
+  }, []); // Empty dependency array means this effect runs once when the component mounts
 
   return (
     <Layout>
@@ -41,16 +40,16 @@ const Section = () => {
                 className="absolute bg-white rounded-md min-w-[1000px] p-2 shadow-md mt-2"
               >
                 {/* Your dropdown content goes here */}
-                {/* {data.map((name, index) => (
+                {data.map((item, index) => (
                   <Link
-                    to={`/coures/${name._id}`} // Assuming you have routes based on the dropdown items
+                    to={`/coures/${item._id}`} // Assuming you have routes based on the dropdown items
                     className="text-[12px] text-black font-bold font-Poppins"
                     key={index}
                   >
-                    {`>   ${name.name}`}
+                    {`>   ${item.name}`}
                     <br />
                   </Link>
-                ))} */}
+                ))}
               </div>
             )}
           </div>
