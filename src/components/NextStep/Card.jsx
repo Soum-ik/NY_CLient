dbb9d066ad280699b9493328ef15558c53fa6f8e
Stack Soumik <110479389+Soum-ik@users.noreply.github.com>
2024-02-07 03:24:29 +0600

    about page time fix

--- src/components/NextStep/Card.jsx
+++ src/components/NextStep/Card.jsx
@@ -8,7 +8,7 @@ const Card = ({ image, name, btn, shortD, id }) => {
      bg-white/70 py-10 duration-500 flex items-center justify-center flex-col space-y-4"
     >
       <img src={image} alt="" />
-      <h3 className=" text-center text-[20px] sm:text-[25px] py-3 font-Headingfont ">
+      <h3 className=" text-center text-[22px] sm:text-[26px] py-3 font-Headingfont ">
         {name}
       </h3>
       <p className=" max-w-xs text-center tracking-wide text-[12px] sm:text-[14px] line-clamp-3">
--- src/components/Timeing/timesection.jsx
+++ src/components/Timeing/timesection.jsx
@@ -21,20 +21,20 @@ function Timesection() {
   return (
     <Layout>
       <Toaster position="top-center" reverseOrder={false} />
-      <div className="sm:px-20 my-10 sm:py-10 sm:flex items-center justify-center gap-20 drop-shadow-lg shadow-lg rounded-lg hidden">
+      <div className="sm:px-20 my-10 sm:py-10 sm:flex items-center justify-center gap-20 drop-shadow-lg shadow-lg rounded-lg ">
         <h1 className="font-Headingfont min-w-max font-bold text-[18px] sm:text-[23px]">
-          Upcoming Courses
+          Upcoming Course
         </h1>
         {click ? (
-          <div className="max-w-sm">
+          <div className="max-w-sm px-1 mt-1">
             <ContactData onSubmit={handleContactDataSubmit} />
           </div>
         ) : (
-          <div className="flex items-center justify-center space-x-20 gap-10">
+          <div className="flex items-center mt-2  overflow-x-scroll sm:overflow-hidden justify-center space-x-20 gap-10">
             {choosepath.upcomingCourses?.map((item, index) => (
               <div
                 key={index}
-                className=" flex flex-col items-center justify-center "
+                className=" flex flex-col sm:first:pl-0 first:pl-20 items-center justify-center "
               >
                 <div className="flex items-center justify-center">
                   {/* Format the start_date and end_date */}
