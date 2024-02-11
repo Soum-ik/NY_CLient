aa181ba6afecc3aa639c31a70fbeb3bea6687f6e
Stack Soumik <110479389+Soum-ik@users.noreply.github.com>
2024-02-11 20:29:40 +0600

    speed up

--- src/libs/paramsData.js
+++ src/libs/paramsData.js
@@ -3,14 +3,14 @@ import { useEffect, useState } from "react";
 
 export function useFetch(id) {
   const [data, setData] = useState([]);
-//   console.log(id.id, "Datat");
   useEffect(() => {
     const getData = async () => {
       const data = await axios.get(`https://node-mongodb-vvwu.onrender.com/choosepath/${id}`);
       setData(data.data[0]);
     };
     getData();
-  }, [id]);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return data;
 }
