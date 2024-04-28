3848c26d001c45a14a7c6849d1e9a04802e2bfe3
Stack Soumik <sarkarsoumik215@gmail.com>
2024-04-28 20:21:49 +0600

    ..

--- src/components/WhatsApp.jsx
+++ src/components/WhatsApp.jsx
@@ -4,7 +4,7 @@ import { BsWhatsapp } from "react-icons/bs";
 const WhatsAppLink = ({ marginTop, textSize }) => {
   // const countryCode = "+880"; // Country code for Bangladesh
   // const phoneNumber = "1754759169"; // Replace with your Grameenphone phone number
-  const message = "Welcome to Farjax Institute"; // Replace with your desired message
+  const message = "Welcome to Farjax Institute of Technology"; // Replace with your desired message
 
   const whatsappUrl = `https://wa.me/${"+19173962689"}?text=${encodeURIComponent(
     message
