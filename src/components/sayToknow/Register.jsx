f8df30ee59d40f74def5f387b6229dc806100522
Stack Soumik <sarkarsoumik215@gmail.com>
2024-06-14 17:50:43 +0600

    register done

--- src/components/sayToknow/Register.jsx
+++ src/components/sayToknow/Register.jsx
@@ -1,17 +1,63 @@
 import { MdEmail } from "react-icons/md";
 import contact from "../../../public/images/contactus.jpg";
-import RegistrationComponent from "./Email";
 import Layout from "../Layout/layout";
+import axios from "axios";
+import config from "../../../config";
+import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 function Register() {
+  const [email, setEmail] = useState("");
+  const Name = "Register User";
+
+  const form = {
+    email,
+    Name,
+  };
+
+  const currentDate = new Date();
+  const formattedDate = `${currentDate.getFullYear()}-${(
+    currentDate.getMonth() + 1
+  )
+    .toString()
+    .padStart(2, "0")}-${currentDate.getDate().toString().padStart(2, "0")}`;
+
+  // Format the time manually
+  const formattedTime = `${currentDate
+    .getHours()
+    .toString()
+    .padStart(2, "0")}:${currentDate.getMinutes().toString().padStart(2, "0")}`;
+
+  const formDataWithDateTime = {
+    ...form,
+    date: formattedDate,
+    time: formattedTime,
+  };
+  const sendToBackend = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+        `${config.apiUrl}application/post`,
+        formDataWithDateTime,
+      );
+      if (data.status === "succes") {
+        toast.success("Your Request Accept succesfully");
+      } else {
+        toast.error("Your Request Accept succesfully");
+      }
+    } catch (error) {
+      toast.error(error);
+    }
+  };
+
   return (
     <Layout>
       <div
         // data-aos="fade-up"
         // data-aos-duration="1000"
-        className="mt-5 flex "
+        className="mt-5 flex"
       >
-        <div className="hidden md:block sm:max-w-[1000px]">
+        <div className="hidden sm:max-w-[1000px] md:block">
           <img
             className="max-h-[400px] min-w-[500px] overflow-hidden object-cover opacity-90"
             src={contact}
@@ -27,21 +73,27 @@ function Register() {
             </p>
           </div>
 
-          <div className="mt-5 grid grid-cols-2 gap-2">
+          <form
+            onSubmit={sendToBackend}
+            className="mt-5 grid grid-cols-2 gap-2 sm:gap-5"
+          >
             <div className="flex items-center rounded-md bg-white py-2 pl-2 pr-3 sm:pr-0">
               <MdEmail className="text-primary" size={22} />
               <input
-                className="min-w-[100px] rounded-md text-sm ml-2 text-black outline-none sm:min-w-[300px]"
+                className="ml-2 min-w-[230px] rounded-md text-sm text-black outline-none md:max-w-[500px] lg:min-w-[300px]"
                 type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 name="user_email"
               />
             </div>
-            <button className=" text-left bg-white text-primary max-w-min p-2 rounded-md">
+            <button className="max-w-min rounded-md bg-white p-2 text-left text-primary">
               Submit
             </button>
-          </div>
+          </form>
         </div>
       </div>
+      <Toaster position="bottom-right" />
     </Layout>
   );
 }
