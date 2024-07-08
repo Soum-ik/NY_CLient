14b04d84fe26888c2b1570aad8018fd9bd86f940
amir047123 <amirfaysal047123@gmail.com>
2024-07-09 04:00:03 +0600

    done

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  // </React.StrictMode>
+  <HelmetProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </HelmetProvider>
 );
