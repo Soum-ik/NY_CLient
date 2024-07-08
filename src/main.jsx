6885ac552ea3d03d9a199f1bb182c960f926bef3
amir047123 <amirfaysal047123@gmail.com>
2024-07-09 04:04:37 +0600

    done

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HelmetProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </HelmetProvider>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
