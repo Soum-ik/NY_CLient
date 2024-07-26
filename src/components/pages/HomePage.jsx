8c5b473b8f65fb4182cf3c7821fc0a853a0ca1ee
Stack Soumik <sarkarsoumik215@gmail.com>
2024-07-26 11:24:29 +0600

    set-up preview image genarator

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -9,23 +9,9 @@ import SayTO from "../sayToknow/Register";
 import PageLayout from "../Layout/PageLayout";
 import NextStep from "../NextStep/NextStep";
 import Contact from "../Contact/Contact";
-import { Helmet } from 'react-helmet';
-
 const HomePage = () => {
   return (
     <div className="">
-
-      <Helmet>
-        <title>Home - Farjax Institute of Technology</title>
-        <meta name="description" content="The Farjax Institute of Technology is a training institute that provides training in various technology-related courses" />
-
-
-        <meta property="og:title" content="Farjax Institute of Technology" />
-        <meta property="og:description" content="The Farjax Institute of Technology is a training institute that provides training in various technology-related courses" />
-        <meta property="og:image" content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwNLcCUBaC99r3nN10jX1asJQgcmxVzbtkgg&s" />
-        <meta property="og:url" content="https://www.farjaxit.com" />
-        <meta property="og:type" content="website" />
-      </Helmet>
       <PageLayout contact={true}>
         <Hero />
         <Explore />
@@ -33,7 +19,7 @@ const HomePage = () => {
         <Contact />
         <Work />
         <SayTO />
-      </PageLayout>  
+      </PageLayout>
 
 
       {/* <div className="  z-50 cursor-pointer h-16 rounded-full w-40  text-center bg-[#06074f]  backdrop-blur-3xl  bottom-20  hover:right-0 ease-in-out duration-300 sm:-right-5 fixed lg:flex shadow-sm  items-center justify-center hidden">
@@ -47,3 +33,4 @@ const HomePage = () => {
   );
 };
 export default HomePage;
+
